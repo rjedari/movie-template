d9fdb8dc25a223c1485029ee9e4d333128f25a47
razie jedari <i.raziyeh@gmail.com>
2023-07-11 13:49:02 +0330

    fix issu

--- src/components/TheInput.vue
+++ src/components/TheInput.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
    <div class="mb-6">
       <label class="block text-gray-700 font-bold mb-2" for="password">
       
@@ -11,6 +12,20 @@
         :placeholder="title"
       />
     </div>
+=======
+  <div class="mb-6">
+     <label class="block text-gray-700 font-bold mb-2" for="password">
+     
+       <slot> </slot>
+     </label>
+     <input
+       class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+       id="password"
+       type="password"
+       :placeholder="title"
+     />
+   </div>
+>>>>>>> 6a989e4 (fix some conflicts)
 </template>
 
 <script setup>
