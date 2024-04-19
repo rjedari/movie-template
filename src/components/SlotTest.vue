d8d08dee908be4ae486fd60b947bb7ab70718a8b
r.jedari <r.jedari@alibaba.ir>
2024-04-19 17:16:56 +0330

    chore: practice slot

---
+++ src/components/SlotTest.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <button class="bg-pink-400 text-white m-5 p-4 rounded-md"> <slot name="header"/> <hr>  <slot message="click" id="20"></slot> <hr> <slot name="footer" id="10"/> </button>
+      
+    </div>
+</template>
+<script setup >
+</script>
--- src/pages/404.vue
+++ src/pages/404.vue
@@ -5,12 +5,41 @@
   <span>: (</span>
   <router-link class="mt-20 text-yellow-600 font-bold hover:text-yellow-500" :to="{ name: 'Home'}">BACK TO HOME
 </router-link>
+  <slot-test>
+   <template #header>
+    <h1 class="text-3xl">H1</h1>
+   </template>
+   <template #default="props"> {{ props.id }} {{ props.message }} on me </template>
+ 
+  <template #footer="props">
+    <h3 class="text-lg">
+      H3<p>{{ props.id }}</p>
+    </h3>
+  </template>
+  </slot-test>
+<div>
+  <p>
+    Ask a yes/no question:
+    <input v-model="question" :disabled="loading" />
+  </p>
+  <p>{{ answer }}</p>
 
+</div>
   </div>
 </template>
 
 <script setup>
+import { computed, reactive, ref, watch } from "vue";
+
 import signInPopUp from '../components/signInPopUp.vue';
+import SlotTest from '../components/SlotTest.vue';
+const question = ref('')
+const answer = ref('Questions usually contain a question mark. ;-)')
+const loading = ref(false)
+watch(question, async(newVal,oldVal)=>{
+  newVal.includes ('?')? `${loading.value === true} , ${answer.value == ' thinking '}` : ' '
+},
+})
 
 </script>
 
